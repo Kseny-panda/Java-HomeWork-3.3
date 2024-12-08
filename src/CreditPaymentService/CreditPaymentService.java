ad65061a9e8beeda2af9eb74a628685b04138f51
Kseniya <i.pretzel16@gmail.com>
2024-12-08 19:59:29 +0500

    commit3

--- src/CreditPaymentService/CreditPaymentService.java
+++ src/CreditPaymentService/CreditPaymentService.java
@@ -4,8 +4,8 @@ public class CreditPaymentService {
 
     public int calculate (double credit, double procent, int years) {
 
-        double quarterlyInterestRate = (procent / 12 / 100);
-        double onePlusInterestRate = (1 + quarterlyInterestRate);
+        double quarterlyInterestRate = (procent / 12 / 100); // ежемесячная ставка
+        double onePlusInterestRate = (1 + quarterlyInterestRate); // 1 + ежемесячная ставка
         double monthlyInterestFactor = Math.pow(onePlusInterestRate, years);
         double payment =  credit * quarterlyInterestRate * monthlyInterestFactor / (monthlyInterestFactor - 1);
 
